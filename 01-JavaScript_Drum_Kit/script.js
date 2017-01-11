e5c1de0335b8da3911be5fc22442ea9bf0923821
Al Hill - Aula Talentum <alvarogil91@gmail.com>
2017-01-11 13:46:06 +0100

    Functional keydown event, unfinished key release function

--- 01-JavaScript_Drum_Kit/script.js
+++ 01-JavaScript_Drum_Kit/script.js
@@ -1,8 +1,19 @@
 function Pulsando(evnt){
 	//console.log(evnt.keyCode);
-	const tecla = document.querySelector('audio[data-key="${evnt.keyCode}"]');
-	console.log(tecla);
+	const sonido = document.querySelector('audio[data-key="'+evnt.keyCode+'"]');
+    const boton = document.querySelector('div[data-key="'+evnt.keyCode+'"]');
+    boton.classList.add("seleccionada");
+    sonido.currentTime = 0;
+    sonido.play();
 }
 
+//INACABADA
+/* function Soltando(evnt){
+    const acabado = document.getElementsByClassName(".seleccionada");
+    acabado.classList.remove("seleccionada");
+} */
+
 window.addEventListener("keydown", Pulsando)
+window.addEventListener("transitionend", Soltando)
+
 
