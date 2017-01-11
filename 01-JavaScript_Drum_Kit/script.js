f989c65eafa10459fbcae2e15f3a69ff034dd595
Álvaro Gil <lvaro Gil>
2017-01-11 20:38:19 +0100

    Functional but with some quirks

--- 01-JavaScript_Drum_Kit/script.js
+++ 01-JavaScript_Drum_Kit/script.js
@@ -1,19 +1,23 @@
-function Pulsando(evnt){
-	//console.log(evnt.keyCode);
+window.addEventListener("keydown",	function Pulsando(evnt){
 	const sonido = document.querySelector('audio[data-key="'+evnt.keyCode+'"]');
-    const boton = document.querySelector('div[data-key="'+evnt.keyCode+'"]');
-    boton.classList.add("seleccionada");
-    sonido.currentTime = 0;
-    sonido.play();
-}
+	const boton = document.querySelector('.button[data-key="'+evnt.keyCode+'"]');
+	boton.classList.add("seleccionada");
+	sonido.currentTime = 0;
+	sonido.play();
+	const teclas = document.querySelectorAll('.button');
+	teclas.forEach(function(item){
+		console.log(item);
+		console.log(boton);
+		item.addEventListener("transitionend", function(item){
+			boton.classList.remove("seleccionada");
+		});
+	});
+	
+});
 
-//INACABADA
-/* function Soltando(evnt){
-    const acabado = document.getElementsByClassName(".seleccionada");
-    acabado.classList.remove("seleccionada");
-} */
 
-window.addEventListener("keydown", Pulsando)
-window.addEventListener("transitionend", Soltando)
+
+
+
 
 
